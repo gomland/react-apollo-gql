e497471cac4e73c4bb3a8c056a6fd5fa10ca3601
gomland <gomland@naver.com>
2019-02-22 12:06:41 +0900

    소스 주석 추가

--- Server/app.js
+++ Server/app.js
@@ -4,6 +4,7 @@ var Graphql = require("graphql");
 var cors = require('cors');
 var dummyDatabase = require("./dummyData.js");
 
+ //더미 데이터의 스키마
 const snackType = new Graphql.GraphQLObjectType({
   name: "Snack",
   fields: {
@@ -17,6 +18,7 @@ const snackType = new Graphql.GraphQLObjectType({
 var queryType = new Graphql.GraphQLObjectType({
   name: "Query",
   fields: {
+    //요청 타입 1 : id를 argument로 받아 조건 매칭
     snacks : {
       type: snackType,
       args: {
@@ -31,6 +33,7 @@ var queryType = new Graphql.GraphQLObjectType({
         return dummyDatabase[data];
       }
     },
+    //요청 타입 2 : 모든 과자의 정보
     allSnacks : {
       type: new Graphql.GraphQLList(snackType),
       resolve: function(obj, args, context, info) {
@@ -44,16 +47,15 @@ var schema = new Graphql.GraphQLSchema({ query: queryType });
 
 var app = express();
 
-app.use(cors());  //로컬 테스트를 위하여 크로스 도메인 허용
-
+app.use(cors());  //로컬 테스트를 위하여 크로스 도메인 허용 설정
 app.use(
-  "/graphql",
+  "/graphql", //라우트 경로 지정
   graphqlHTTP({
     schema: schema,
     graphiql: true
   })
 );
 
-app.listen(4000);
+app.listen(4000); //4000 포트로 실행
 
 console.log("Running a GraphQL server");
